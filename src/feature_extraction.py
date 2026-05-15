f0697b9b941c4a8e260ce9a51af9d9b3801afbc9
Duygu <dggecer@gmail.com>
2026-05-16 00:27:19 +0300

    ✧ Improve EEG classification accuracy using normalization

--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -3,9 +3,27 @@ import mne
 import numpy as np
 import pandas as pd
 
+from scipy.signal import welch
+
 DATA_PATH = "data/raw/physionet_eeg"
 
 all_features = []
+
+
+def bandpower(data, sf, band):
+
+    low, high = band
+
+    freqs, psd = welch(data, sf)
+
+    freq_resolution = freqs[1] - freqs[0]
+
+    idx_band = np.logical_and(freqs >= low, freqs <= high)
+
+    power = np.sum(psd[idx_band]) * freq_resolution
+
+    return power
+
 
 for subject_folder in os.listdir(DATA_PATH):
 
@@ -25,24 +43,31 @@ for subject_folder in os.listdir(DATA_PATH):
 
                 data = raw.get_data()
 
-                mean_value = np.mean(data)
-                std_value = np.std(data)
-                max_value = np.max(data)
-                min_value = np.min(data)
+                sf = raw.info['sfreq']
+
+                flattened_data = data.flatten()
+
+
+                delta = bandpower(flattened_data, sf, (0.5, 4))
+                theta = bandpower(flattened_data, sf, (4, 8))
+                alpha = bandpower(flattened_data, sf, (8, 13))
+                beta = bandpower(flattened_data, sf, (13, 30))
+
 
                 all_features.append({
                     "subject": subject_folder,
-                    "mean": mean_value,
-                    "std": std_value,
-                    "max": max_value,
-                    "min": min_value
+                    "delta": delta,
+                    "theta": theta,
+                    "alpha": alpha,
+                    "beta": beta
                 })
 
 
 df = pd.DataFrame(all_features)
 
-print("\n=== EXTRACTED FEATURES ===\n")
+print("\n=== EXTRACTED EEG BAND FEATURES ===\n")
 print(df.head())
+
 
 df.to_csv("data/processed/features.csv", index=False)
 
--- src/train_model.py
+++ src/train_model.py
@@ -3,12 +3,16 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 
 df = pd.read_csv("data/processed/features.csv")
 print("\n=== LOADED FEATURES ===\n")
 print(df.head())
 
-X = df[["mean", "std", "max", "min"]]
+X = df[["delta", "theta", "alpha", "beta"]]
+scaler = StandardScaler()
+X = scaler.fit_transform(X)
+
 y = df["subject"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -21,5 +25,8 @@ predictions = model.predict(X_test)
 
 accuracy = accuracy_score(y_test, predictions)
 
+print("\n=== SUBJECT DISTRIBUTION ===")
+print(df["subject"].value_counts())
+
 print("\n<=== MODEL ACCURACY ===>\n")
-print(f"    Accuracy: {accuracy * 100:.2f}%")
+print(f"      Accuracy: {accuracy * 100:.2f}%")
